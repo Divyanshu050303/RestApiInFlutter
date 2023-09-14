066c61f0333673ff7bb7ff8d8027441a769e0b65
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-09-14 21:36:57 +0530

    Upload Photo post request

---
+++ lib/UploadPhoto_postRequest.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostRequest extends StatefulWidget {
+  const PostRequest({super.key});
+
+  @override
+  State<PostRequest> createState() => _PostRequestState();
+}
+
+class _PostRequestState extends State<PostRequest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
