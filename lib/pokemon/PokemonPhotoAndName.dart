f2e514831131ce960b5777cb5ed73d2ac8280cc7
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-09-17 00:29:38 +0530

    Combine all the details

--- lib/pokemon/PokemonPhotoAndName.dart
+++ lib/pokemon/PokemonPhotoAndName.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'as http;
-import 'package:trysomethingnew/Models/pokehub.dart';
+import 'package:http/http.dart' as http;
+import 'package:trysomethingnew/pokemon/PokemonDetails.dart';
 
-import '../Models/pokehub.dart';
+import '../Models/pokemons.dart';
+
 class PokemonNameAndPhoto extends StatefulWidget {
   const PokemonNameAndPhoto({super.key});
 
@@ -13,81 +15,73 @@ class PokemonNameAndPhoto extends StatefulWidget {
 }
 
 class _PokemonNameAndPhotoState extends State<PokemonNameAndPhoto> {
- var url="https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-     late Pokehub pokehub;
-@override
+  Future<List<Pokemon>> fetchPokemonList() async {
+    print("Function Called");
+    final response = await http.get(
+      Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'),
+    );
+    print("Url");
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> results = data['results'];
+      List<Pokemon> pokemonList = [];
+      for (Map result in results) {
+        final pokemonResponse = await http.get(Uri.parse(result['url']));
+        if (pokemonResponse.statusCode == 200) {
+          final Map<String, dynamic> pokemonData =
+              json.decode(pokemonResponse.body);
+          pokemonList.add(Pokemon.fromJson(pokemonData));
+        }
+      }
+      print(pokemonList.length);
+      return pokemonList;
+    } else {
+      throw Exception('Failed to load Pokémon');
+    }
+  }
+
+  List<Pokemon> pokemonList = [];
+
+  @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    fetctData();
+    fetchPokemonList().then((list) {
+      setState(() {
+        pokemonList = list;
+        pokemonList.shuffle();
+      });
+    });
   }
-  fetctData() async{
-  var res=await http.get(Uri.parse(url));
-  var decoded=jsonDecode(res.body);
-  pokehub=Pokehub.fromJson(decoded);
-  setState(() {
 
-  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Pokémon List'),
+        ),
+        body: GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemCount: pokemonList.length,
+            itemBuilder: (context, index) {
+              return Hero(
+                tag:pokemonList[index].imageUrl ,
+                child: GridTile(child: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>PokemonDetails(pokemon: pokemonList, index: index,)));
+                  },
+                  child: Card(
+                    child:Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                       children: [
+                         Image.network(pokemonList[index].imageUrl,height: 100,width: 120,),
+                         Text(pokemonList[index].name, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),)
+                       ],
+
+                    )
+                  ),
+                )),
+              );
+            }));
   }
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: const Text("Poke App"),
-       backgroundColor: Colors.cyan,
-     ),
-     body: pokehub == null
-         ? const Center(
-       child: CircularProgressIndicator(),
-     )
-         : GridView.count(
-       crossAxisCount: 2,
-       children: pokehub.pokemon
-           !.map((poke) => Padding(
-         padding: const EdgeInsets.all(2.0),
-         child: InkWell(
-           onTap: () {
-             // Navigator.push(
-             //     context,
-             //     MaterialPageRoute(
-             //         builder: (context) => PokeDetail(
-             //           pokemon: poke,
-             //         )));
-           },
-           child: Hero(
-             tag: poke.img.toString(),
-             child: Card(
-               child: Column(
-                 mainAxisSize: MainAxisSize.max,
-                 mainAxisAlignment:
-                 MainAxisAlignment.spaceEvenly,
-                 children: <Widget>[
-                   Container(
-                     height: MediaQuery.of(context).size.height *
-                         0.4,
-                     width:
-                     MediaQuery.of(context).size.width * 0.2,
-                     decoration: BoxDecoration(
-                         image: DecorationImage(
-                             fit: BoxFit.cover,
-                             image: NetworkImage(poke.img.toString()))),
-                   ),
-                   Text(
-                     poke.name.toString(),
-                     style: const TextStyle(
-                       fontSize: 20.0,
-                       fontWeight: FontWeight.bold,
-                     ),
-                   )
-                 ],
-               ),
-             ),
-           ),
-         ),
-       ))
-           .toList(),
-     ),
-
-   );
- }
 }
