10f454f3a9a0aa92b8968bfe64740ab3e826d082
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-10-06 20:39:43 +0530

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,121 @@
+// import 'package:flutter/material.dart';
+// import 'package:trysomethingnew/HomePaage.dart';
+// import 'package:trysomethingnew/Provider/ProviderClass.dart';
+// import 'package:trysomethingnew/pokemon/PokemonPhotoAndName.dart';
+// import 'package:provider/provider.dart' ;
+// void main() {
+//   runApp(const MyApp());
+// }
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (BuildContext context) { CatConv(); },
+//       child: MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//           useMaterial3: true,
+//         ),
+//         home: const HomePage(),
+//         debugShowCheckedModeBanner: false,
+//       ),
+//     );
+//   }
+// }
+//
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:trysomethingnew/pokemon/PokemonPhotoAndName.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+
+
+import 'Models/cart.dart';
+import 'Models/catalog.dart';
+import 'Screen/cart.dart';
+import 'Screen/catalog.dart';
+import 'Screen/login.dart';
+import 'common.dart';
+
 void main() {
+  // setupWindow();
   runApp(const MyApp());
 }
-class MyApp extends StatefulWidget {
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+
+
+GoRouter router() {
+  return GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const MyLogin(),
+      ),
+      GoRoute(
+        path: '/catalog',
+        builder: (context, state) => const MyCatalog(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            builder: (context, state) => const MyCart(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        theme: appTheme,
+        routerConfig: router(),
       ),
-      home: const PokemonNameAndPhoto(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
-
