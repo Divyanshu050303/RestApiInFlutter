f01f5e7e37619094201f064d41b7b9fb13dc3b9f
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-20 20:19:53 +0530

    Internationalization class

---
+++ lib/getx/InternationalizationClass.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+
+class Internationalizations extends Translations{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+    'en_US':{
+      'hello':'jai shree ram'
+    },
+    'hi_IN':{
+      'hello':'जय श्री राम'
+    },
+    'fr_FR':{
+      'hello':'jai shree bélier'
+    }
+
+  };
+
+}
